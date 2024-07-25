1cea0f57d1a4d3f84318e22d09a891dc46907c44
Billy King <74845157+verike1996@users.noreply.github.com>
2024-07-25 12:53:12 -0400

    initializing a repo for the SSBM character selection screen

---
+++ js/main.js
@@ -0,0 +1,103 @@
+let body = document.querySelector('section')
+// event listeners for each li
+document.querySelector('#drMario').addEventListener('click', drMarioBG)
+document.querySelector('#mario').addEventListener('click', marioBG)
+document.querySelector('#luigi').addEventListener('click', luigiBG)
+document.querySelector('#bowser').addEventListener('click', bowserBG)
+document.querySelector('#peach').addEventListener('click', peachBG)
+document.querySelector('#yoshi').addEventListener('click', yoshiBG)
+document.querySelector('#dk').addEventListener('click', dkBG)
+document.querySelector('#captainFalcon').addEventListener('click', captainFalconBG)
+document.querySelector('#ganon').addEventListener('click', ganonBG)
+document.querySelector('#falco').addEventListener('click', falcoBG)
+document.querySelector('#fox').addEventListener('click', foxBG)
+document.querySelector('#ness').addEventListener('click', nessBG)
+document.querySelector('#icies').addEventListener('click', iciesBG)
+document.querySelector('#kirby').addEventListener('click', kirbyBG)
+document.querySelector('#samus').addEventListener('click', samusBG)
+document.querySelector('#zeldaSheik').addEventListener('click', zeldaSheikBG)
+document.querySelector('#link').addEventListener('click', linkBG)
+document.querySelector('#youngLink').addEventListener('click', youngLinkBG)
+document.querySelector('#pichu').addEventListener('click', pichuBG)
+document.querySelector('#pikachu').addEventListener('click', pikachuBG)
+document.querySelector('#jPuff').addEventListener('click', jPuffBG)
+document.querySelector('#mewtwo').addEventListener('click', mewtwoBG)
+document.querySelector('#mrGaW').addEventListener('click', mrGaWBG)
+document.querySelector('#marth').addEventListener('click', marthBG)
+document.querySelector('#roy').addEventListener('click', royBG)
+
+function drMarioBG() {
+    body.style.backgroundImage = "url(assets/drMarioBG.png)"
+}
+function marioBG() {
+    body.style.backgroundImage = "url(assets/MarioBG.png)"
+}
+function luigiBG() {
+    body.style.backgroundImage = "url(assets/luigiBG.png)"
+}
+function bowserBG() {
+    body.style.backgroundImage = "url(assets/bowserBG.png)"
+}
+function peachBG() {
+    body.style.backgroundImage = "url(assets/peachBG.png)"
+}
+function yoshiBG() {
+    body.style.backgroundImage = "url(assets/yoshiBG.png)"
+}
+function dkBG() {
+    body.style.backgroundImage = "url(assets/dkBG.png)"
+}
+function captainFalconBG() {
+    body.style.backgroundImage = "url(assets/captainFalconBG.png)"
+}
+function ganonBG() {
+    body.style.backgroundImage = "url(assets/ganonBG.png)"
+}
+function falcoBG() {
+    body.style.backgroundImage = "url(assets/falcoBG.png)"
+}
+function foxBG() {
+    body.style.backgroundImage = "url(assets/foxBG.png)"
+}
+function nessBG() {
+    body.style.backgroundImage = "url(assets/nessBG.png)"
+}
+function iciesBG() {
+    body.style.backgroundImage = "url(assets/iciesBG.png)"
+}
+function kirbyBG() {
+    body.style.backgroundImage = "url(assets/kirbyBG.png)"
+}
+function samusBG() {
+    body.style.backgroundImage = "url(assets/samusBG.png)"
+}
+function zeldaSheikBG() {
+    body.style.backgroundImage = "url(assets/zeldaBG.png)"
+}
+function linkBG() {
+    body.style.backgroundImage = "url(assets/linkBG.png)"
+}
+function youngLinkBG() {
+    body.style.backgroundImage = "url(assets/youngLinkBG.png)"
+}
+function pichuBG() {
+    body.style.backgroundImage = "url(assets/pichuBG.png)"
+}
+function pikachuBG() {
+    body.style.backgroundImage = "url(assets/pikachuBG.png)"
+}
+function jPuffBG() {
+    body.style.backgroundImage = "url(assets/jPuffBG.png)"
+}
+function mewtwoBG() {
+    body.style.backgroundImage = "url(assets/mewtwoBG.png)"
+}
+function mrGaWBG() {
+    body.style.backgroundImage = "url(assets/mrGaWBG.png)"
+}
+function marthBG() {
+    body.style.backgroundImage = "url(assets/marthBG.png)"
+}
+function royBG() {
+    body.style.backgroundImage = "url(assets/royBG.png)"
+}
